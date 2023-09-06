6f01dd574f57d972b071e72c12ca8c0bdf5cccaa
badx86 <badlolpro@gmail.com>
2023-09-06 16:08:28 +0400

    add comments

--- ResetStrategies/NoReset.py
+++ ResetStrategies/NoReset.py
@@ -1,4 +1,5 @@
 from appium import webdriver
+
 # Do not stop app, do not clear app data, and do not unistall apk
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
@@ -10,3 +11,7 @@ desired_caps['appActivity'] = 'com.google.android.apps.chrome.Main'
 desired_caps['noReset'] = True
 
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+
+# No Reset (noReset): Этот флаг гарантирует, что приложение не будет сбрасываться перед началом новой сессии,
+# сохраняя все данные и состояния. Это полезно, если вы хотите продолжить тестирование с того места, где остановились
+# в предыдущей сессии.
