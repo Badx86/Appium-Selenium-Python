c1aa7d1d67c84f1bf835e5519d27f6ebe08b5a07
badx86 <badlolpro@gmail.com>
2023-09-18 22:04:42 +0400

    add Python BDD section part2

---
+++ SeleniumBDDFW/environment.py
@@ -0,0 +1,20 @@
+from base.BasePage import BaseClass
+from base.DriverClass import WebDriverClass
+import utilities.CustomLogger as cl
+import configurationfiles.DeviceConfig as dc
+
+log = cl.customLogger()
+
+
+def before_all(context):
+    log.info("Automation Started")
+    context.testVariable = "This is a Test variable"
+    context.driver1 = WebDriverClass()
+    context.driver = context.driver1.getWebDriver(dc.bName)
+    context.bp = BaseClass(context.driver)
+    context.bp.launchWebPage(dc.url, dc.title)
+
+
+def after_all(context):
+    context.driver.quit()
+    log.info("Automation Ended")
